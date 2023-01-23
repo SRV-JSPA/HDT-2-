228fb6f8774fd368fb1bfb2cdd861dfb2b5c7a86
nancygmm <99931623+nancygmm@users.noreply.github.com>
2023-01-23 11:21:02 -0600

    Encabezados

---
+++ Main.java
@@ -0,0 +1,12 @@
+/*
+Profesor: Douglas Leonel Barrios
+Auxiliar: Fernanda Esquivel y Francisco Castillo
+INTEGRANTES: 
+José Santiago Pereira Alvarado, 22318
+Gustavo Adolfo Cruz Bardales, 22779
+Nancy Gabriela Mazariegos Molina, 22513
+27/01/2023
+Hoja de Trabajo 2
+Curso: Algoritmos y Estructuras de datos
+Sección: 20
+*/
