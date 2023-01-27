9e732b9adf563c62e56d2be721d4f62e1c008318
G2309 <106942151+G2309@users.noreply.github.com>
2023-01-27 06:45:06 -0600

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -21,33 +21,9 @@ public interface Stack <Integer> {
  * Interface que representa a un Stack.
  */
 
-    public void add (Integer item);
-   /**
-    * pre:
-    * post: item is added to stack
-    * will be popped next if no intervening push
-    * 
-    * @param item item to push to the stack.
-    */
-   public void push(Integer item);
-
-   /**
-    * pre: stack is not empty
-    * post: most recently pushed item is removed and returned
-    * 
-    * @return the element in the top of the stack.
-    * @throws EmptyStackException If the stack is empty.
-    */
-   public Integer pop() throws EmptyStackException;
-
-   /**
-    * pre: stack is not empty
-    * post: top value (next to be popped) is returned
-    * 
-    * @return the element in the top of the stack.
-    * @throws EmptyStackException If the stack is empty.
-    */
-
+    double evaluate(final String p0) throws ArithmeticException;
+	    
+	double getPrevious();
 
 
 }
